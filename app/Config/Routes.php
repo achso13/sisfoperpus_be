40e78aa91cea84d222d3f8b9e5747558b3f5f1f6
Dany Sentiana <danysentiana1200@gmail.com>
2023-05-08 20:54:39 +0700

    Fix API response

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,7 +31,7 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 
-$routes->post('/register', 'Register::index');
+$routes->post('/register', 'Auth::register');
 
 /*
  * --------------------------------------------------------------------
--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -6,16 +6,15 @@ use App\Controllers\BaseController;
 use App\Models\UserModel;
 use CodeIgniter\API\ResponseTrait;
 
-class Register extends BaseController
+class Auth extends BaseController
 {
     use ResponseTrait;
 
-    public function index()
+    public function register()
     {
         $rules = [
             'username' => 'required|min_length[5]|max_length[20]|is_unique[user.username]',
             'password' => 'required|min_length[8]|max_length[255]',
-            'password_confirm' => 'required|matches[password]',
             'email' => 'required|valid_email|is_unique[user.email]',
             'role' => 'required|in_list[ANGGOTA,ADMIN]'
         ];
@@ -25,7 +24,7 @@ class Register extends BaseController
 
             $userData = [
                 'username' => $this->request->getVar('username'),
-                'password' => password_hash($this->request->getVar('password'), PASSWORD_BCRYPT),
+                'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
                 'email' => $this->request->getVar('email'),
                 'role' => $this->request->getVar('role')
             ];
@@ -33,12 +32,18 @@ class Register extends BaseController
             $userModel->save($userData);
 
             return $this->respond([
-                'status' => 200,
-                'message' => 'User berhasil ditambahkan'
+                'status' => 'success',
+                'message' => 'User berhasil ditambahkan',
+                'data' => [
+                    'user_id' => $userModel->getInsertID()
+                ]
+
             ], 200);
+
         } else {
             $response = [
-                'status' => 400,
+                'status' => 'failed',
+                'message' => 'User gagal ditambahkan',
                 'error' => $this->validator->getErrors()
             ];
 
